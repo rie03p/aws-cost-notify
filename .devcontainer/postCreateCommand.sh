74e8ae20d1060c18dc56cf34a5be7b8d3ad7c1c3
rie03p <kotoganaiyo@gmail.com>
2025-12-06 07:33:36 +0000

    devcontainer

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env zsh
+
+set -exo pipefail
+
+if ! grep -q mise ~/.zshrc; then
+    # shellcheck disable=SC2016
+    echo 'eval "$(mise activate zsh)"' >> ~/.zshrc
+fi
+
+mise trust
+mise install -y
+eval "$(mise activate zsh)"
+eval "$(mise hook-env)"
+
+npm install -g aws-cdk
+
